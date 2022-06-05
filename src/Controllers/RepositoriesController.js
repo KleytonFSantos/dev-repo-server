0f96792e810c10d0c400b2067e73aed3058f649d
kleytondev <akatsukipb12@gmail.com>
2022-06-04 23:59:54 -0300

    crud repositories

---
+++ src/Controllers/RepositoriesController.js
@@ -0,0 +1,85 @@
+import User from "../models/User";
+import Repository from "../models/Repository";
+
+class RepositoriesController {
+    async index(req, res) {
+        try {
+            const { user_id } = req.params
+            const user = await User.find({ user_id });
+
+            if(!user){
+                return res.status(404).json();
+            }
+
+            const repositories = await Repository.find({
+                userId: user_id,
+            });
+
+            return res.json(repositories);
+        } catch (error){
+            console.error(error);
+            return res.status(500).json({ error: "Internal server error." });
+        }
+    }
+
+    async create(req, res) {
+        try {
+            const { user_id } = req.params
+            const { name, url } = req.body
+
+            const user = await User.find({ user_id });
+
+            if(!user){
+                return res.status(404).json();
+            }
+
+            const repository = await Repository.findOne({
+                userId: user_id,
+                name,
+            })
+
+            if (repository){
+                return res.status(422).json({ message: `Repository ${name} already exists.`});
+            }
+            
+            const newRepository = await Repository.create({
+                name,
+                url,
+                userId: user_id,
+            });
+
+            return res.status(201).json({ message: `Repository ${name} was created successfully.`})
+        } catch (error){
+            console.error(error);
+            return res.status(500).json({ error: "Internal server error." });
+        }
+    }
+
+    async destroy(req, res){
+        try {
+            const { user_id, id } = req.params
+
+            const user = await User.find({ user_id });
+
+            if(!user){
+                return res.status(404).json();
+            }
+
+            const repository = await Repository.findOne({
+                id,
+                userId: user_id
+            });
+            if(!repository){
+                return res.status(404).json();
+            }
+            await repository.deleteOne()
+
+            return res.status(200).json({ message: `Repository ${repository.name} was deleted successfully.`});
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ error: "Internal server error." });
+        }
+    }
+}
+
+export default new RepositoriesController();
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
+import auth from './middlewares/auth'
 import HelloController from './Controllers/HelloController';
 import UsersController from './Controllers/UsersController';
+import SessionsController from './Controllers/SessionsController';
+import RepositoriesController from './Controllers/RepositoriesController';
 
 const routes = new Router();
 
 routes.get('/hello', HelloController.index);
 
+routes.post('/sessions', SessionsController.create);
+
+routes.use(auth);
 
 routes.get('/users', UsersController.index);
 routes.get('/users/:id', UsersController.show);
@@ -13,5 +19,12 @@ routes.get('/users/:id', UsersController.show);
 routes.post('/users', UsersController.create);
 routes.put('/users/:id', UsersController.update);
 routes.delete('/users/:id', UsersController.destroy);
+
+routes.get('/users/:user_id/repositories', 
+    RepositoriesController.index);
+routes.post('/users/:user_id/repositories',
+    RepositoriesController.create);
+routes.delete('/users/:user_id/repositories', 
+    RepositoriesController.destroy);
 
 export default routes;
